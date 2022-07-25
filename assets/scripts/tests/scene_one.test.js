3c724a362199e02399f8de3dfa87e4ccb06b3b9c
Claire221 <c.biglands@hotmail.com>
2022-07-25 10:01:53 +0000

    Adds code to test file

--- assets/scripts/tests/scene_one.test.js
+++ assets/scripts/tests/scene_one.test.js
@@ -1,1 +1,10 @@
-const test = require("../scene_one");
+/* jshint esversion: 11 */
+
+// module.exports is required to export the objects and functions to the Jest testing file.
+// The if statement prevents this from logging an error in the browser console.
+// https://stackoverflow.com/a/68671391
+if (typeof module !== "undefined") module.exports = {
+    const test = require("../scene_one");
+};
+
+// when start game is called clitch is 0 time is 0
