59ad452207cb29e21ca9b46f600c031a6c55c930
Claire221 <c.biglands@hotmail.com>
2022-07-26 13:11:24 +0000

    Add jest test

--- assets/scripts/tests/scene_one.test.js
+++ assets/scripts/tests/scene_one.test.js
@@ -1,10 +1,51 @@
-/* jshint esversion: 11 */
+/**
+ * @jest-environment jsdom
+ */
+
 
 // module.exports is required to export the objects and functions to the Jest testing file.
 // The if statement prevents this from logging an error in the browser console.
 // https://stackoverflow.com/a/68671391
-if (typeof module !== "undefined") module.exports = {
-    const test = require("../scene_one");
-};
+// if (typeof module !== "undefined") module.exports = {
+//     const test = require(".scene_one");
+
+    
+// };
 
 // when start game is called clitch is 0 time is 0
+
+// const testVariable = require("../stage_functions");
+
+// beforeEach(() => {
+//     let fs = require("fs");
+//     let fileContents = fs.readFileSync("index.html", "utf-8");
+//     document.open();
+//     document.write(fileContents);
+//     document.close();
+// })
+
+// describe("testVariable", () => {
+//     describe("testVariable value", () => {
+//         test("testVariable should be equal to 7", () => {
+//             expect(testVariable).toEqual(7);
+//         })
+//     });
+// });
+
+const labCoat = require("../stage_functions");
+
+beforeEach(() => {
+    let fs = require("fs");
+    let fileContents = fs.readFileSync("index.html", "utf-8");
+    document.open();
+    document.write(fileContents);
+    document.close();
+})
+
+describe("labCoat", () => {
+    describe("labCoat value", () => {
+        test("labCoat should be false", () => {
+            expect(labCoat).toBe(false);
+        })
+    });
+});
